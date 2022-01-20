3ee9a030fa350b5c7fd7e4b6bf52a3c6f9c438c9
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-20 13:33:23 +0000

    ./filesList /home

--- fileList.c
+++ fileList.c
@@ -1,61 +1,16 @@
-#include <dirent.h>
-#include "cstring.h"
-char *fileLists(char *directory){
+#include "fileList.h"
+
+int main(int argc,char *argv[]){
+	char *d="\ec\e[44;36m";
+	//list all files in argv 1
 	char *lst;
-	struct dirent *ldir;
-	char *s;
-	int lens=0;
-	int total=0;
-	char r[2080];
-	lst=NULL;
-	DIR *dir=opendir(directory);
-	if(dir==NULL)return lst;
-	//list directorys
-	ldir=readdir(dir);
-	while(ldir!=NULL){
-		if(ldir->d_name!=NULL){
-			lens=strlen(ldir->d_name);
-			if (lens>2070)lens=2070;
-			strncpy(r,ldir->d_name,(int)lens+1);
-			r[lens];
-			if (total==0){
-				if (ldir->d_type==DT_DIR){
-					lst=newList(r);
-					total++;
-				}
-			}else{
-				if (ldir->d_type==DT_DIR){
-					lst=addList(lst,r);
-					total++;
-				}
-			}
-			ldir=readdir(dir);
-		}
+	int n=0;
+	char *dirs=".";
+	print(d);
+	if (argc>1){
+		lst=fileLists(argv[1]);
+		printList(lst);
+		freeList(lst);
 	}
-	//list files
-	dir=opendir(directory);
-	if(dir==NULL)return lst;
-	ldir=readdir(dir);
-	while(ldir!=NULL){
-		if(ldir->d_name!=NULL){
-			lens=strlen(ldir->d_name);
-			if (lens>2070)lens=2070;
-			strncpy(r,ldir->d_name,(int)lens+1);
-			r[lens];
-			if (total==0){
-				if (ldir->d_type!=DT_DIR){
-					lst=newList(r);
-					total++;
-				}
-			}else{
-				if (ldir->d_type!=DT_DIR){
-					lst=addList(lst,r);
-					total++;
-				}
-			}
-			
-			ldir=readdir(dir);
-		}
-	}
-	return lst;
+	return 0;
 }
