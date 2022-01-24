765344ba82bef3297deb449a9d0a09b8b1ec6d0f
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-24 18:11:57 +0000

    ./loadtxt awk.txt

--- loadtxt.c
+++ loadtxt.c
@@ -1,10 +1,14 @@
 #include "fileList.h"
 int main(int argc,char *argv[]){
 	char *cc;
+	char *ccc;
 	printf("\ec\e[44;36m\a\n\n");
 	if(argc>1){
 		cc=loadtxt(argv[1],5000008);
-		print(cc);
+		ccc=splitString(cc,'\n');
+		printList(ccc);
+		frees(ccc);
+		frees(cc);
 	}
 	return 0;
 }
